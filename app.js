fe0c0d9fcb216133b7fa2f0227b5b667197d0b84
Dadalifeee <mathias.lunardon@free.fr>
2020-07-02 16:14:43 +0200

    xwxw

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ var app = express();
 
     server = require('http').createServer(app),
     io = require('socket.io').listen(server),
-    ent = require('ent') // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
+    ent = require('ent') 
 app.use(express.static('public'));
-// Chargement de la page index.html
 app.get('/', function (req, res) {
   res.sendfile('index.html');
 });
@@ -15,14 +14,12 @@ app.get('/chat2', function (req, res) {
   });
 
 io.sockets.on('connection', function (socket, pseudo) {
-    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
     socket.on('nouveau_client', function(pseudo) {
         pseudo = ent.encode(pseudo);
         socket.pseudo = pseudo;
         socket.broadcast.emit('nouveau_client', pseudo);
     });
 
-    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
     socket.on('message', function (message) {
         message = ent.encode(message);
         socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
